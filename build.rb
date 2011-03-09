abab5ae8c9b98532a74b2712a2bed42a6b1a288c
ippa <iplord@iplord.com>
2011-03-09 23:11:13 +0100

    Run jaws-all.js through googles closure compiler to create jaws-min.js, currently taking up ~16k

--- build.rb
+++ build.rb
@@ -1,10 +1,32 @@
 #!/usr/bin/env ruby
 
 #
-# Built an all-containing jaws.js with all src files
+# Build standalone,all-including jaws-all.js by combining all files in src/-directory into one
 #
 out = File.new("jaws-all.js", "w")
 Dir["src/*"].each do |file|
   out.write(File.read(file))
 end
+out.close
+
+#
+# Minify jaws-all.js into jaws-min.js using googles closure compiler
+#
+require 'net/http'
+require 'uri'
+def compress(js_code, compilation_level)
+  response = Net::HTTP.post_form(URI.parse('http://closure-compiler.appspot.com/compile'), {
+    'js_code' => js_code,
+    'compilation_level' => "#{compilation_level}",
+    'output_format' => 'text',
+    'output_info' => 'compiled_code'
+  })
+  response.body
+end
+
+js_code = File.read("jaws-all.js")
+
+out = File.new("jaws-min.js", "w")
+out.write compress(js_code, "SIMPLE_OPTIMIZATIONS") # ADVANCED_OPTIMIZATIONS
+out.close
 
