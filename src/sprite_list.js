1f253ff4a4f2d74f22ba5d600d6480b79b6537af
ippa <iplord@iplord.com>
2011-03-26 01:42:29 +0100

    replace reserved word delete with remove

--- src/sprite_list.js
+++ src/sprite_list.js
@@ -23,7 +23,7 @@ var jaws = (function(jaws) {
 jaws.SpriteList = function() {}
 jaws.SpriteList.prototype = new Array
 
-jaws.SpriteList.prototype.delete = function(obj) {
+jaws.SpriteList.prototype.remove = function(obj) {
   var index = this.indexOf(obj)
   if(index > -1) { this.splice(index, 1) }
 }
