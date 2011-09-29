95d8f9675d5aec26ca1a6576acd42eaea40178ea
ippa <iplord@iplord.com>
2011-09-29 17:50:13 +0200

    support commonjs in sprite

--- src/sprite.js
+++ src/sprite.js
@@ -346,6 +346,8 @@ jaws.Sprite.prototype.toJSON = function() {
 return jaws;
 })(jaws || {});
 
+// Support CommonJS require()
+if(typeof module !== "undefined" && ('exports' in module)) { module.exports = jaws.Sprite }
 
 /*
 // Chainable setters under consideration:
