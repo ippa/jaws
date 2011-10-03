11a32cc7d4b6472df202b39baa46871736b4d3b0
ippa <iplord@iplord.com>
2011-10-03 21:51:16 +0200

    build

--- jaws.js
+++ jaws.js
@@ -581,7 +581,8 @@ jaws.Assets = function Assets() {
    */
   this.add = function(src) {
     if(jaws.isArray(src)) { for(var i=0; src[i]; i++) { this.add(src[i]) } }
-    else                  { var path = this.root + src; this.src_list.push(path) }
+    else                  { this.src_list.push(src) }
+    // else                  { var path = this.root + src; this.src_list.push(path) }
     return this
   }
  
@@ -616,7 +617,7 @@ jaws.Assets = function Assets() {
 
     switch(this.getType(asset.src)) {
       case "image":
-        var src = asset.src + "?" + parseInt(Math.random()*10000000)
+        var src = this.root + asset.src + "?" + parseInt(Math.random()*10000000)
         asset.image = new Image()
         asset.image.asset = asset // enables us to access asset in the callback
         //
@@ -627,7 +628,7 @@ jaws.Assets = function Assets() {
         asset.image.src = src
         break;
       case "audio":
-        var src = asset.src + "?" + parseInt(Math.random()*10000000)
+        var src = this.root + asset.src + "?" + parseInt(Math.random()*10000000)
         asset.audio = new Audio(src)
         asset.audio.asset = asset         // enables us to access asset in the callback
         this.data[asset.src] = asset.audio
@@ -636,7 +637,7 @@ jaws.Assets = function Assets() {
         asset.audio.load()
         break;
       default:
-        var src = asset.src + "?" + parseInt(Math.random()*10000000)
+        var src = this.root + asset.src + "?" + parseInt(Math.random()*10000000)
         var req = new XMLHttpRequest()
         req.asset = asset         // enables us to access asset in the callback
         req.onreadystatechange = this.assetLoaded
@@ -2100,6 +2101,51 @@ return jaws;
 
 // Support CommonJS require()
 if(typeof module !== "undefined" && ('exports' in module)) { module.exports = jaws.TileMap }
+/*
+ * 
+ *  Add collision detection helpers to "jaws"-object
+ *
+ */
+var jaws = (function(jaws) {
+
+/** 
+ * Returns true if circles collide.
+ * Takes two objects with properties "radius" as argument.
+ */
+jaws.collideCircles = function(object1, object2) {
+  return ( jaws.distanceBetween(object1, object2) < object1.radius + object2.radius )
+}
+
+/** 
+ * returns the distance between 2 objects
+ */
+jaws.distanceBetween = function(object1, object2) {
+  return Math.sqrt( Math.pow(object1.x-object2.x, 2) +  Math.pow(object1.y.object2.y, 2) )
+}
+
+/*
+ * Collides 2 objects or list with objects.
+ * Returns empty array if no collision took place
+ * Returns array of array with object-pairs that collided
+ *
+ * @examples
+ *   jaws.collide(player, enemy)
+ *   jaws.collide(player, enemies)   // --> [player, enemies[2]]
+ *   jaws.collide(bullets, enemies) 
+ *   jaws.collide(player, ememies)
+ *
+ */
+jaws.collide = function(object1, object2) {
+  if(object1.radius && object2.radius && jaws.collideCircles(object1, object2))  { return [object1, object2]; }
+  if(object1.rect && object2.rect && object1.rect().collideRect(object2.rect())) { return [object1, object2]; }
+  if(object1.forEach) return object1.map( function(item1) { jaws.collide(item1, object2) } );
+  if(object2.forEach) return object2.map( function(item2) { jaws.collide(item2, object1) } );
+  return false;
+}
+
+return jaws;
+})(jaws || {});
+
 var jaws = (function(jaws) {
   /**
   * @namespace GFX related helpers
