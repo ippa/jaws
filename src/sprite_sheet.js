85acb519b8e63d49aef750e22f0c1ba6340b6ff0
ippa <iplord@iplord.com>
2011-10-04 16:39:43 +0200

    orientation-parameter for SpriteSheet(), cut tiles 'right' or 'down'

--- src/sprite_sheet.js
+++ src/sprite_sheet.js
@@ -18,7 +18,7 @@ function cutImage(image, x, y, width, height) {
  * @class Cut out invidual frames (images) from a larger spritesheet-image
  *
  * @property {image|image} Image/canvas or asset-string to cut up smaller images from
- * @property {string} orientation How to cut out invidual images from spritesheet, either "left" or "right"
+ * @property {string} orientation How to cut out invidual images from spritesheet, either "right" or "down"
  * @property {array} frame_size  width and height of invidual frames in spritesheet
  * @property {array} frames all single frames cut out from image
 */
@@ -26,7 +26,7 @@ jaws.SpriteSheet = function SpriteSheet(options) {
   if( !(this instanceof arguments.callee) ) return new arguments.callee( options );
 
   this.image = jaws.isDrawable(options.image) ? options.image : jaws.assets.data[options.image]
-  this.orientation = options.orientation || "right"
+  this.orientation = options.orientation || "down"
   this.frame_size = options.frame_size || [32,32]
   this.frames = []
   
@@ -38,9 +38,21 @@ jaws.SpriteSheet = function SpriteSheet(options) {
   }
 
   var index = 0
-  for(var x=0; x < this.image.width; x += this.frame_size[0]) {
+
+  // Cut out tiles from Top -> Bottom
+  if(this.orientation == "down") {  
+    for(var x=0; x < this.image.width; x += this.frame_size[0]) {
+      for(var y=0; y < this.image.height; y += this.frame_size[1]) {
+        this.frames.push( cutImage(this.image, x, y, this.frame_size[0], this.frame_size[1]) )
+      }
+    }
+  }
+  // Cut out tiles from Left -> Right
+  else {
     for(var y=0; y < this.image.height; y += this.frame_size[1]) {
-      this.frames.push( cutImage(this.image, x, y, this.frame_size[0], this.frame_size[1]) )
+      for(var x=0; x < this.image.width; x += this.frame_size[0]) {
+        this.frames.push( cutImage(this.image, x, y, this.frame_size[0], this.frame_size[1]) )
+      }
     }
   }
 }
