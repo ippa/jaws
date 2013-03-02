63178b4430173e175266667b9dedc27b899c2d17
ippa <iplord@iplord.com>
2013-03-02 21:43:39 +0100

    test spritesheet framesize autodetection

--- test/sprite_sheet.js
+++ test/sprite_sheet.js
@@ -15,6 +15,10 @@ test("SpriteSheet", function() {
     equal(sprite_sheet.frames[0].toString(), "[object HTMLCanvasElement]", "one frame is canvas")
     equal(sprite_sheet.frames.length, 14, "correct number of frames")
     // equal(sprite_sheet.frames.length, sprite_sheet.length, "frames.length == length")
+    
+    sprite_sheet = new jaws.SpriteSheet( {image: "droid_11x15.png"} )
+    same(sprite_sheet.frame_size, [11,15], "frame_size autodetection from filename")
+
     start();
   }
 })
