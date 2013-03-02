efc75c569148ded9eef3343e817c27bf882152ee
ippa <iplord@iplord.com>
2013-03-02 17:26:38 +0100

    anchor -> setAnchor, add assetsloading

--- test/sprite.js
+++ test/sprite.js
@@ -26,65 +26,71 @@ test("Sprite without image", function() {
 });
 
 test("Sprite", function() {
-  sprite = new jaws.Sprite({image: "assets/rect.png", x:0, y:0})
-  equal(sprite.width, 20, "sprite.width")  
-  equal(sprite.height, 20, "sprite.height")
+  jaws.assets.add("assets/rect.png")
+  jaws.assets.loadAll({onfinish:assetsLoaded})
+  stop();
 
-  sprite.scaleAll(2)
-  equal(sprite.rect().width, 40, "sprite.rect().width after scaling x2")
-  equal(sprite.rect().height, 40, "sprite.rect().height after scaling x2") 
-
-  // console.log(sprite.x + " - " + sprite.scale_x + " - " + sprite.anchor_x)
-  same(sprite.rect(), new jaws.Rect(0,0,40,40), "sprite.rect()")
-
-  sprite.anchor("bottom_right")
-  equal(sprite.x, sprite.rect().right, "sprite.x == sprite.rect().right when anchor is bottom_right")
-  equal(sprite.y, sprite.rect().bottom, "sprite.y == sprite.rect().bottom when anchor is bottom_right")
+  function assetsLoaded() {
+    sprite = new jaws.Sprite({image: "assets/rect.png", x:0, y:0})
+    equal(sprite.width, 20, "sprite.width")  
+    equal(sprite.height, 20, "sprite.height")
   
-  sprite.anchor("top_left")
-  equal(sprite.x+sprite.width, sprite.rect().right, "sprite.x+sprite.width == sprite.rect().right when anchor is top_left")
-  equal(sprite.y+sprite.height, sprite.rect().bottom, "sprite.y+sprite.height == sprite.rect().bottom when anchor is top_left") 
-
-  sprite.rotateTo(45)
-  equal(sprite.angle, 45, "sprite.rotateTo() modifies angle")
-  sprite.rotate(45)
-  equal(sprite.angle, 90, "sprite.rotate() adds to angle #2")
-
-  sprite.moveTo(100,100)
-  equal(sprite.x, 100, "sprite.moveTo() sets sprite x/y")
-  equal(sprite.y, 100, "sprite.moveTo() sets sprite x/y")
-
-  sprite.move(10,12)
-  equal(sprite.x, 110, "sprite.move() adds to sprite x/y")
-  equal(sprite.y, 112, "sprite.move() adds to sprite x/y")
-
-  sprite.scaleTo(1)
-  equal(sprite.width, 20, "sprite.scaleTo forces a scale_factor")
-
-  sprite.setWidth(80)
-  equal(sprite.width, 80, "sprite.setWidth forces a new width via scale_x")
-  equal(sprite.scale_x, 4, "sprite.setWidth forces a new width via scale_x")
-
-  sprite.setHeight(40)
-  equal(sprite.height, 40, "sprite.setHeight forces a new width via scale_y")
-  equal(sprite.scale_y, 2, "sprite.setHeight forces a new width via scale_y")
-
-  sprite.resizeTo(20,20)
-  equal(sprite.width, 20, "resize() sets width via scale_x")
-  equal(sprite.height, 20, "resize() sets width via scale_x")
+    sprite.scaleAll(2)
+    equal(sprite.rect().width, 40, "sprite.rect().width after scaling x2")
+    equal(sprite.rect().height, 40, "sprite.rect().height after scaling x2") 
   
-  sprite.resize(-10,-10)
-  equal(sprite.width, 10, "resize() mods width via scale_x")
-  equal(sprite.height, 10, "resize() mods width via scale_x")
-
-  var flipped = sprite.flipped
-  sprite.flip()
-  equal(sprite.flipped, !flipped, "sprite.flip inverts flipped")
-  sprite.flip()
-  equal(sprite.flipped, flipped, "sprite.flip inverts flipped")
-
-  sprite2 = new jaws.Sprite({image: "rect.png", scale_image: 2})
-  equal(sprite2.width, 40, "Sprite({scale_image: 2}) and sprite.width")  
-  equal(sprite2.height, 40, "Sprite({scale_image: 2}) and sprite.height")
+    // console.log(sprite.x + " - " + sprite.scale_x + " - " + sprite.anchor_x)
+    same(sprite.rect(), new jaws.Rect(0,0,40,40), "sprite.rect()")
+  
+    sprite.setAnchor("bottom_right")
+    equal(sprite.x, sprite.rect().right, "sprite.x == sprite.rect().right when anchor is bottom_right")
+    equal(sprite.y, sprite.rect().bottom, "sprite.y == sprite.rect().bottom when anchor is bottom_right")
+    
+    sprite.setAnchor("top_left")
+    equal(sprite.x+sprite.width, sprite.rect().right, "sprite.x+sprite.width == sprite.rect().right when anchor is top_left")
+    equal(sprite.y+sprite.height, sprite.rect().bottom, "sprite.y+sprite.height == sprite.rect().bottom when anchor is top_left") 
+  
+    sprite.rotateTo(45)
+    equal(sprite.angle, 45, "sprite.rotateTo() modifies angle")
+    sprite.rotate(45)
+    equal(sprite.angle, 90, "sprite.rotate() adds to angle #2")
+  
+    sprite.moveTo(100,100)
+    equal(sprite.x, 100, "sprite.moveTo() sets sprite x/y")
+    equal(sprite.y, 100, "sprite.moveTo() sets sprite x/y")
+  
+    sprite.move(10,12)
+    equal(sprite.x, 110, "sprite.move() adds to sprite x/y")
+    equal(sprite.y, 112, "sprite.move() adds to sprite x/y")
+  
+    sprite.scaleTo(1)
+    equal(sprite.width, 20, "sprite.scaleTo forces a scale_factor")
+  
+    sprite.setWidth(80)
+    equal(sprite.width, 80, "sprite.setWidth forces a new width via scale_x")
+    equal(sprite.scale_x, 4, "sprite.setWidth forces a new width via scale_x")
+  
+    sprite.setHeight(40)
+    equal(sprite.height, 40, "sprite.setHeight forces a new width via scale_y")
+    equal(sprite.scale_y, 2, "sprite.setHeight forces a new width via scale_y")
+  
+    sprite.resizeTo(20,20)
+    equal(sprite.width, 20, "resize() sets width via scale_x")
+    equal(sprite.height, 20, "resize() sets width via scale_x")
+    
+    sprite.resize(-10,-10)
+    equal(sprite.width, 10, "resize() mods width via scale_x")
+    equal(sprite.height, 10, "resize() mods width via scale_x")
+  
+    var flipped = sprite.flipped
+    sprite.flip()
+    equal(sprite.flipped, !flipped, "sprite.flip inverts flipped")
+    sprite.flip()
+    equal(sprite.flipped, flipped, "sprite.flip inverts flipped")
+  
+    sprite2 = new jaws.Sprite({image: "rect.png", scale_image: 2})
+    equal(sprite2.width, 40, "Sprite({scale_image: 2}) and sprite.width")  
+    equal(sprite2.height, 40, "Sprite({scale_image: 2}) and sprite.height")
+  }
 })
 
