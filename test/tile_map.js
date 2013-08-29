65bedb1002f5cdeaf9a89e8f30cfa03111c0eac1
ippa <iplord@iplord.com>
2013-08-29 16:39:02 +0200

    fix tests

--- test/tile_map.js
+++ test/tile_map.js
@@ -1,14 +1,15 @@
 module("TileMap");
 
 test("TileMap", function() {
-  jaws.assets.root = "assets/"
-  jaws.assets.add("rect.png")
-  jaws.assets.loadAll({onload: assetsLoaded})
   stop();
+  var assets = new jaws.Assets();
+  assets.setRoot("assets/").add("rect.png").loadAll({onload: assetsLoaded})
 
   function assetsLoaded() { 
+    var image = assets.get("rect.png");
+
     var tile_map = new jaws.TileMap({size: [10,10], cell_size: [32,32]})
-    var sprite = new jaws.Sprite({image: "rect.png", x: 40, y: 40, anchor: "top_left"})
+    var sprite = new jaws.Sprite({image: image, x: 40, y: 40, anchor: "top_left"})
     tile_map.push(sprite)
     deepEqual(tile_map.cells[0][0], [], "empty")
     deepEqual(tile_map.at(10,10), [], "empty coordinates returns nil")
@@ -16,16 +17,14 @@ test("TileMap", function() {
     deepEqual(tile_map.cell(0,0), [], "empty cell")
     deepEqual(tile_map.cell(1,1)[0], sprite, "occupied cell returns correct object")
     
-    var sprite2 = new jaws.Sprite({image: "rect.png", x: 45, y: 45, anchor: "top_left"})
-    var sprite3 = new jaws.Sprite({image: "rect.png", x: 45, y: 45, anchor: "top_left"})
+    var sprite2 = new jaws.Sprite({image: image, x: 45, y: 45, anchor: "top_left"})
+    var sprite3 = new jaws.Sprite({image: image, x: 45, y: 45, anchor: "top_left"})
     tile_map.push(sprite2)
     tile_map.push(sprite3)
     ok(jaws.isArray(tile_map.at(40,40)), "returns array of sprites")
     deepEqual(tile_map.cell(1,1), [sprite, sprite2, sprite3], "array of sprites occupying cell 1,1")
     start();
   }
-
-  // console.log(tile_map.cell(1,1))
 })
 
 test("TileMap advanced", function() {
@@ -46,19 +45,22 @@ test("TileMap advanced", function() {
 })
 
 test("TileMap simple usage", function () {
-    var tile_map = new jaws.TileMap({size: [10, 10], cell_size: [32, 32]})
-    for (var i= 0; i < 10; ++i) {
-	for (var j = 0; j < 10; ++j) {
-	    tile_map.push(new jaws.Sprite({image: "rect.png", x: i * 32, y: j * 32, anchor: "top_left"}))
-	}
-    }
-    var top_left = new jaws.Rect(0, 0, 5 * 32, 5 * 32);
-    ok(tile_map.atRect(top_left).length >= 25, "There should be at least 25 tiles showing")
-    var top_right = new jaws.Rect(5 * 32, 0, 5 * 32, 5 * 32);
-    ok(tile_map.atRect(top_right).length >= 25, "There should be at least 25 tiles showing")
-    var bottom_left = new jaws.Rect(0, 5 * 32, 5 * 32, 5 * 32);
-    ok(tile_map.atRect(bottom_left).length >= 25, "There should be at least 25 tiles showing")
-    var bottom_right = new jaws.Rect(5 * 32, 5 * 32, 5 * 32, 5 * 32);
-    ok(tile_map.atRect(bottom_right).length >= 25, "There should be at least 25 tiles showing")
+  var tile_map = new jaws.TileMap({size: [10, 10], cell_size: [32, 32]});
+  for (var i= 0; i < 10; ++i) {
+  	for (var j = 0; j < 10; ++j) {
+     tile_map.push(new jaws.Sprite({image: "rect.png", x: i * 32, y: j * 32, anchor: "top_left"}));
+	  }
+  }
+  var top_left = new jaws.Rect(0, 0, 5 * 32, 5 * 32);
+  ok(tile_map.atRect(top_left).length >= 25, "There should be at least 25 tiles showing")
+  
+  var top_right = new jaws.Rect(5 * 32, 0, 5 * 32, 5 * 32);
+  ok(tile_map.atRect(top_right).length >= 25, "There should be at least 25 tiles showing")
+  
+  var bottom_left = new jaws.Rect(0, 5 * 32, 5 * 32, 5 * 32);
+  ok(tile_map.atRect(bottom_left).length >= 25, "There should be at least 25 tiles showing")
+  
+  var bottom_right = new jaws.Rect(5 * 32, 5 * 32, 5 * 32, 5 * 32);
+  ok(tile_map.atRect(bottom_right).length >= 25, "There should be at least 25 tiles showing")
 })
 
